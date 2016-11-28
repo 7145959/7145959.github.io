827e0dae51944b445af2daaf11d47aecb7c57a52
7145959 <claire@iropke.com>
2016-11-28 22:36:53 +0900

    test

---
+++ DAY13/js/toggleGrid.js
@@ -0,0 +1,11 @@
+(function(global){
+  'use strict';
+  var doc = global.document;
+  var body = doc.querySelector('body');
+  doc.addEventListener('keydown', function(e) {
+    if (e.ctrlKey) { body.classList.toggle('overlay'); }
+    if (e.keyCode === 71 && e.shiftKey) {
+      body.classList.toggle('show-grid');
+    }
+  });
+})(this);
